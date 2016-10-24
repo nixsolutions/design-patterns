c51d48a3c91605dc018aa52d389d714f94a7b98d
Phil Sushko <dasd_90@hotmail.com>
2016-10-24 10:36:50 +0300

    Code Clean

--- patterns/Creational/SimpleFactory/SimpleFactory.php
+++ patterns/Creational/SimpleFactory/SimpleFactory.php
@@ -5,7 +5,7 @@ class SimpleFactory implements SimpleFactoryInterface
 {
     public static function create(string $productType): ProductInterface
     {
-        $className = __NAMESPACE__ . '\\' .  $productType;
+        $className = __NAMESPACE__ . '\\' . $productType;
         return new $className;
     }
 }
