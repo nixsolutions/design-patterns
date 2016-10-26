5cee9d2946043c8e45ec59b8d1c0d958b685e6b7
Phil Sushko <dasd_90@hotmail.com>
2016-10-26 13:36:16 +0300

    Clear code

--- patterns/Creational/Singleton/Advance/IvanPassport.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace DesignPatterns\Creational\Singleton\Advance;
-
-class IvanPassport extends PassportSingletonAbstract
-{
-    protected $firstName = 'Ivan';
-    protected $lastName = 'Ivanov';
-    protected $birthDay = '1970-01-01';
-}
