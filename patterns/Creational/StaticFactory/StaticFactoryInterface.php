d4aac8cff69f34258b39423a3bd60054cd4a8a70
Phil Sushko <dasd_90@hotmail.com>
2016-11-02 18:37:53 +0200

    Delete old example

--- patterns/Creational/StaticFactory/StaticFactoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace DesignPatterns\Creational\StaticFactory;
-
-use DesignPatterns\Creational\StaticFactory\Books\AbstractBook;
-
-interface StaticFactoryInterface
-{
-    public static function build(string $bookName): AbstractBook;
-}
