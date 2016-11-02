93e855049c217fe224115ed5a93a97c7d8a474b7
Phil Sushko <dasd_90@hotmail.com>
2016-11-02 13:13:01 +0200

    Delete old example for Factory method

--- patterns/Creational/FactoryMethod/Factories/KharkivBookFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace DesignPatterns\Creational\FactoryMethod\Factories;
-
-class KharkivBookFactory extends BookFactory
-{
-    const CITY = 'Kharkiv';
-}
--- patterns/Creational/FactoryMethod/Factories/LvivBookFactory.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace DesignPatterns\Creational\FactoryMethod\Factories;
-
-class LvivBookFactory extends BookFactory
-{
-    const CITY = 'Lviv';
-}
