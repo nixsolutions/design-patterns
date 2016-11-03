a57cb4f63b84d1a29d1b51b2a5b232b7c7451837
Phil Sushko <dasd_90@hotmail.com>
2016-11-03 17:37:23 +0200

    Delete old example for Builder

--- patterns/Creational/Builder/Burger/Burger.php
+++ patterns/Creational/Builder/Burger/Burger.php
@@ -1,33 +1,7 @@
 <?php
+
 namespace DesignPatterns\Creational\Builder\Burger;
 
-class Burger
+class Burger extends AbstractBurger
 {
-    private $sauce;
-    private $meat;
-    private $muffin;
-
-    /**
-     * @param string $sauce
-     */
-    public function setSauce(string $sauce)
-    {
-        $this->sauce = $sauce;
-    }
-
-    /**
-     * @param string $meat
-     */
-    public function setMeat(string $meat)
-    {
-        $this->meat = $meat;
-    }
-
-    /**
-     * @param string $muffin
-     */
-    public function setMuffin(string $muffin)
-    {
-        $this->muffin = $muffin;
-    }
 }
